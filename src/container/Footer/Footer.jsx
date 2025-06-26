88d16cd09770180585ed778b4c42ccc5e777a86f
Plovaxs <143068878+Plovaxs@users.noreply.github.com>
2025-06-27 00:19:58 +0700

    Update contact info

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="joshabrahamefendi2005@gmail.com" className="p-text">joshabrahamefendi2005@gmail.com</a>
+          <a href="mailto:joshabrahamefendi2005@gmail.com" className="p-text">joshabrahamefendi2005@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
